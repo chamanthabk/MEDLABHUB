98d70631a60a7d1b255331664bdaa188cced0bac
jaya0232 <jaya0232@flinders.edu.au>
2023-09-06 11:55:47 +0930

    homepage changes

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,5 +2,5 @@ function login() {
     // Normally, you'd validate user credentials here.
     // But for this example, we'll skip that part.
     
-    window.location.href = "main.html";
+    window.location.href = "/Pages/homepage.html";
 }
