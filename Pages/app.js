f2bc641cb0dcda77e714650f87dbc724db92d206
jaya0232 <142423836+jaya0232@users.noreply.github.com>
2023-09-06 14:55:23 +0930

    Add files via upload

---
+++ Pages/app.js
@@ -0,0 +1,18 @@
+
+function login() {
+    window.location.href = "/Pages/homepage.html";
+}
+
+// const sign_in_btn = document.querySelector("#sign-in-btn");
+// const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+// sign_up_btn.addEventListener('click', () =>{
+//     container.classList.add("sign-up-mode");
+// });
+
+// sign_in_btn.addEventListener('click', () =>{
+//     window.location.href = "/Pages/homepage.html";
+//     container.classList.remove("sign-up-mode");
+// });
+
---
+++ Pages/script.js
@@ -0,0 +1,6 @@
+function login() {
+    // Normally, you'd validate user credentials here.
+    // But for this example, we'll skip that part.
+    
+    window.location.href = "/Pages/homepage.html";
+}
