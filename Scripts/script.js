d8fbec1045612222d559e6c2f223f592d50f2f95
jaya0232 <jaya0232@flinders.edu.au>
2023-09-07 16:42:29 +0930

    homepage changes, doctor changes

--- Scripts/script.js
+++ Scripts/script.js
@@ -4,3 +4,18 @@ function login() {
     
     window.location.href = "/Pages/homepage.html";
 }
+
+function enterdoctordashboard() {
+    // Normally, you'd validate user credentials here.
+    // But for this example, we'll skip that part.
+    
+    window.location.href = "/Pages/Doctor/doctorDashboard.html";
+}
+
+
+function viewPatients() {
+    // Normally, you'd validate user credentials here.
+    // But for this example, we'll skip that part.
+    
+    window.location.href = "/Pages/Doctor/viewPatients.html";
+}
